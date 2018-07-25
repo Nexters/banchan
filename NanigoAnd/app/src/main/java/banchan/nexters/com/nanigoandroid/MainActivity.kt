af315449f7fdb1146d957b5bc545faa651574f9b
jiyoung <ji9310@naver.com>
2018-07-25 14:06:36 +0900

    slack test commit

--- NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/MainActivity.kt
+++ NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/MainActivity.kt
@@ -2,11 +2,17 @@ package banchan.nexters.com.nanigoandroid
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btn_hello.text = "Hi! banchan"
+        btn_hello.setOnClickListener { Toast.makeText(this@MainActivity, "btn_hello Click", Toast.LENGTH_SHORT).show() }
     }
 }
