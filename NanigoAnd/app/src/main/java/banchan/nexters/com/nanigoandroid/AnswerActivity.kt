967c48622e3633eecf603c15eee23963fddcfc09
ellen <dhy039@gmail.com>
2018-07-31 16:22:51 +0900

    답변 Activity 그리는중

---
+++ NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/AnswerActivity.kt
@@ -0,0 +1,12 @@
+package banchan.nexters.com.nanigoandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AnswerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+    }
+}
