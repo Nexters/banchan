9280dedf406dd4cebc61ae35c50fb9519fd85871
Jiyoung <ji9310@naver.com>
2018-08-19 20:25:48 +0900

    카드 UI 구성 완료 - Type A, B, C, D

--- NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/adapter/SnappyAdapter.kt
+++ NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/adapter/SnappyAdapter.kt
@@ -2,73 +2,91 @@ package banchan.nexters.com.nanigoandroid.adapter
 
 import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
+import banchan.nexters.com.nanigoandroid.R
 import banchan.nexters.com.nanigoandroid.animation.FlipAnimation
+import banchan.nexters.com.nanigoandroid.item.TestItem
 import banchan.nexters.com.nanigoandroid.listener.FlipListener
-import banchan.nexters.com.nanigoandroid.viewholder.SnappyViewHolder
-import java.text.ParseException
-import java.text.SimpleDateFormat
 import java.util.*
 
 
 class SnappyAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(), FlipListener {
 
+
+    val VIEW_TYPE_A = 0
+    val VIEW_TYPE_B = 1
+    val VIEW_TYPE_C = 3
+    val VIEW_TYPE_D = 4
+
     lateinit var mCard: FrameLayout
     lateinit var mImageView: ImageView
     lateinit var mRootLayout: ConstraintLayout
 
+    private val mItems = ArrayList<TestItem>()
 
+    init{
+        mItems.add(TestItem("첫번째 질문", null, VIEW_TYPE_A))
+        mItems.add(TestItem("두번째 질문", null, VIEW_TYPE_B))
+        mItems.add(TestItem("세번째 질문", null, VIEW_TYPE_C))
+        mItems.add(TestItem("네번째 질문", null, VIEW_TYPE_D))
+        mItems.add(TestItem("다섯번쨰 질문", null, VIEW_TYPE_A))
+        mItems.add(TestItem("여섯번째 질문", null, VIEW_TYPE_B))
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return SnappyViewHolder(parent!!)
+
+        return if (viewType == VIEW_TYPE_A) {
+            AHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_question_card, parent, false))
+        } else if (viewType == VIEW_TYPE_B){
+            BHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_question_card_b, parent, false))
+        } else if (viewType == VIEW_TYPE_C) {
+            CHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_question_card_c, parent, false))
+        } else {
+            DHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_question_card_d, parent, false))
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         //(holder as SnappyViewHolder).setDate(dates[position])
-        mCard = (holder as SnappyViewHolder).getCardView()
-        mImageView = (holder as SnappyViewHolder).getImageView()
-        mRootLayout = (holder as SnappyViewHolder).getRootLayout()
+        /*mCard = (holder as SnappyViewHolder).getCardView()
+        mCard.tag = ("mCard")
+        mImageView = holder.getImageView()
+        mImageView.tag = ("mImageView")
+        mRootLayout = holder.getRootLayout()
+        mRootLayout.tag = ("mRootLayout")*/
+
+        val test = mItems[position]
+        if (holder is AHolder) {
+            (holder as AHolder).mQuestion.text = test.question
+        } else if (holder is BHolder){
+            (holder as BHolder).mQuestion.text = test.question
+        } else if (holder is CHolder){
+            (holder as CHolder).mQuestion.text = test.question
+        } else {
+            (holder as DHolder).mQuestion.text = test.question
+        }
 
     }
 
-    private var startDate: Date? = null
-    private var endDate: Date? = null
-
-    private var dates: MutableList<Date>
-
-    init {
-        val formatter = SimpleDateFormat("yyyy-MM-dd")
-
-        try {
-            startDate = formatter.parse("2017-01-01")
-        } catch (ex: ParseException) {
-        }
-
-        try {
-            endDate = formatter.parse("2018-12-31")
-        } catch (ex: ParseException) {
-        }
-
-        val start = Calendar.getInstance()
-        start.time = startDate
-
-        val end = Calendar.getInstance()
-        end.time = endDate
-
-        dates = ArrayList()
-        var date = start.time
-        while (start.before(end)) {
-            dates.add(date)
-            start.add(Calendar.DATE, 1)
-            date = start.time
+    override fun getItemViewType(position: Int): Int {
+        return if (mItems.get(position).item_viewtype == VIEW_TYPE_A) {
+            VIEW_TYPE_A
+        } else if (mItems.get(position).item_viewtype == VIEW_TYPE_B){
+            VIEW_TYPE_B
+        } else if (mItems.get(position).item_viewtype == VIEW_TYPE_C){
+            VIEW_TYPE_C
+        } else {
+            VIEW_TYPE_D
         }
     }
 
     override fun getItemCount(): Int {
-        return dates.size
+        return mItems.size
     }
 
     fun getCardView() : FrameLayout {
@@ -97,5 +115,63 @@ class SnappyAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(), FlipListe
         return this
     }
 
+    inner class AHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+        var mCard: FrameLayout
+        var mImageView: ImageView
+        var mRootLayout: ConstraintLayout
+        var mQuestion: TextView
+
+        init {
+            mCard = itemView.findViewById(R.id.cv_question_card)
+            mImageView = itemView.findViewById(R.id.O_img)
+            mRootLayout = itemView.findViewById(R.id.cl_card)
+            mQuestion = itemView.findViewById(R.id.tv_question)
+        }
+    }
+
+    inner class BHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var mCard: FrameLayout
+        var mImageView: ImageView
+        var mRootLayout: ConstraintLayout
+        var mQuestion: TextView
+
+        init {
+            mCard = itemView.findViewById(R.id.cv_question_card)
+            mImageView = itemView.findViewById(R.id.O_img)
+            mRootLayout = itemView.findViewById(R.id.cl_card)
+            mQuestion = itemView.findViewById(R.id.tv_question)
+        }
+    }
+
+    inner class CHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var mCard: FrameLayout
+        var mImageView: ImageView
+        var mRootLayout: ConstraintLayout
+        var mQuestion: TextView
+
+        init {
+            mCard = itemView.findViewById(R.id.cv_question_card)
+            mImageView = itemView.findViewById(R.id.O_img)
+            mRootLayout = itemView.findViewById(R.id.cl_card)
+            mQuestion = itemView.findViewById(R.id.tv_question)
+        }
+    }
+
+    inner class DHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var mCard: FrameLayout
+        var mImageView: ImageView
+        var mRootLayout: ConstraintLayout
+        var mQuestion: TextView
+
+        init {
+            mCard = itemView.findViewById(R.id.cv_question_card)
+            mImageView = itemView.findViewById(R.id.O_img)
+            mRootLayout = itemView.findViewById(R.id.cl_card)
+            mQuestion = itemView.findViewById(R.id.tv_question)
+        }
+    }
 }
---
+++ NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/item/TestItem.kt
@@ -0,0 +1,7 @@
+package banchan.nexters.com.nanigoandroid.item
+
+/**
+ * Created by XNOTE on 2018-08-19.
+ */
+data class TestItem(var question: String, var q_image: String?, var item_viewtype: Int) {
+}
