e088a8d2bb5de8b9721074c180b237896f827726
ellen <dhy039@gmail.com>
2018-08-20 17:58:07 +0900

    로딩 유틸 추가 deviceKey 유틸 추가 preference 유틸 추가

---
+++ NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/utils/PreferenceManager.java
@@ -0,0 +1,117 @@
+package banchan.nexters.com.nanigoandroid.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * This class is for use sharedPreferences to easy on evenywhere.
+ * Implement singleton pattern. If you want add value.
+ * You can modify sample code.
+ * <p/>
+ * Sampe code. (You will change all key(KEY,Key,key) to value name use before)
+ * private  String KEY = "key";
+ * private String key = "";
+ * <p/>
+ * public void setKey(String key){
+ * this.key = key;
+ * mEditor.putString(KEY,key);
+ * mEditor.apply();
+ * }
+ * <p/>
+ * public String getKey(){
+ * if(key.equals("")){
+ * key = mPrefs.getString("KEY", "");
+ * }
+ * return key;
+ * }
+ * //background select
+ * private  String BGSELECT = "bgSelect";
+ * private int bgSelect = 1;
+ * <p/>
+ * public void setBgSelect(int bgSelect){
+ * this.bgSelect = bgSelect;
+ * mEditor.putInt(BGSELECT, bgSelect);
+ * mEditor.apply();
+ * }
+ * <p/>
+ * public int getBgSelect(){
+ * if(bgSelect == 1){
+ * bgSelect = mPrefs.getInt(BGSELECT, 1);
+ * }
+ * return bgSelect;
+ * }
+ */
+
+/*
+ *getpref
+ * PreferenceManager.getInstance().getPhonenum()
+ *
+ * setpref
+ * PreferenceManager.getInstance().setPphoneNum(phone);
+ *
+ */
+
+
+
+public class PreferenceManager {
+    private String PREF_NAME = "nanigo_prefs.xml";
+
+    private static PreferenceManager instance;
+    private SharedPreferences mPrefs;
+    private SharedPreferences.Editor mEditor;
+
+    public static PreferenceManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new PreferenceManager(context);
+        }
+        return instance;
+    }
+
+
+    private PreferenceManager(Context context) {
+        mPrefs = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        mEditor = mPrefs.edit();
+    }
+
+
+
+
+    private String UUID = "UUID";
+
+    public void setUuid(String key) {
+        mEditor.putString(UUID, key);
+        mEditor.apply();
+    }
+
+    public String getUuid() {
+        return mPrefs.getString(UUID, "");
+    }
+
+
+
+    private String USERID = "USERID";
+
+    public void setUserId(String key) {
+        mEditor.putString(USERID, key);
+        mEditor.apply();
+    }
+
+    public String getUserId() {
+        return mPrefs.getString(USERID, "");
+    }
+
+
+
+
+    //clear
+    public void clear(String key) {
+        mEditor.remove(key);
+        mEditor.apply();
+    }
+
+    public void allClear() {
+        mEditor = mPrefs.edit();
+        mEditor.clear();
+        mEditor.apply();
+    }
+}
