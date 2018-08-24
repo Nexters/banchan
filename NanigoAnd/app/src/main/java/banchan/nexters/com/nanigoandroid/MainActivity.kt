ac1192a92676400ebfc83b9924ff61e19a39a0f5
Jiyoung <ji9310@naver.com>
2018-08-25 03:56:27 +0900

    TabLayout 이미지 변경, viewPager fadein fadeout 적용

--- NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/MainActivity.kt
+++ NanigoAnd/app/src/main/java/banchan/nexters/com/nanigoandroid/MainActivity.kt
@@ -2,9 +2,12 @@ package banchan.nexters.com.nanigoandroid
 
 import android.os.Bundle
 import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import banchan.nexters.com.nanigoandroid.adapter.MainPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 
 
@@ -21,9 +24,15 @@ class MainActivity : AppCompatActivity(){
         main_tablayout.addTab(main_tablayout.newTab().setIcon(R.drawable.selector_tab_logo))
         main_tablayout.addTab(main_tablayout.newTab().setIcon(R.drawable.selector_tab_mypage))
         main_tablayout.tabGravity = TabLayout.GRAVITY_FILL
-        for (i in 0 until main_tablayout.getTabCount()) {
+        for (i in 0 until main_tablayout.tabCount) {
             val tab = main_tablayout.getTabAt(i)
-            if (tab != null) tab.setCustomView(R.layout.layout_tab_view)
+            if (tab != null) {
+                when(i) {
+                    0 -> tab.setCustomView(R.layout.layout_tab_speaker)
+                    1 -> tab.setCustomView(R.layout.layout_tab_view)
+                    2 -> tab.setCustomView(R.layout.layout_tab_mypage)
+                }
+            }
         }
 
         main_viewpager.adapter = MainPagerAdapter(supportFragmentManager)
@@ -44,8 +53,24 @@ class MainActivity : AppCompatActivity(){
             }
         })
 
+        main_viewpager.setPageTransformer(false, NoPageTransformer())
 
+    }
 
+    inner class NoPageTransformer : ViewPager.PageTransformer {
+        override fun transformPage(view: View, position: Float) {
+            if(position <= -1.0F || position >= 1.0F) {
+                view.translationX = view.width * position
+                view.alpha = 0.0F
+            } else if( position == 0.0F ) {
+                view.translationX = view.width * position
+                view.alpha = 1.0F
+            } else {
+                // position is between -1.0F & 0.0F OR 0.0F & 1.0F
+                view.translationX = view.width * -position
+                view.alpha = 1.0F - Math.abs(position)
+            }
+        }
     }
 
 }
